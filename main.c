580830004c53b76df66717e224a7ad3eeef670d5
annisahaura <annisa355@mhs.usk.ac.id>
2024-12-06 10:48:53 +0700

    Update main.c

--- main.c
+++ main.c
@@ -1,22 +1,27 @@
-#include "header.h"             //berisi protipe dan fungsi yg diperlukan
+#include "header.h"             // Berisi prototipe dan fungsi yang diperlukan
 #include <stdio.h>
 #include <string.h>
 
-int main() {
+int main(int argc, char *argv[]) {
     Alat alat[100];
     Pinjaman pinjaman[100];
     int jumlahAlat = 0, jumlahPinjaman = 0;
     char username[50], password[50], role[10];
 
-    printf("Masukkan username: ");
-    scanf("%s", username);
-    printf("Masukkan password: ");
-    scanf("%s", password);
+    // Memeriksa jumlah argumen yang diberikan
+    if (argc != 3) {
+        printf("Penggunaan: %s <username> <password>\n", argv[0]);
+        return 1; // Mengembalikan kode kesalahan
+    }
+
+    // Mengambil username dan password dari argumen command line
+    strcpy(username, argv[1]);
+    strcpy(password, argv[2]);
 
     // Melakukan login
-    if (login(username, password, role) == 1) {         //jika pengguna memasukkan data yg benar maka program login akan dijalankan
+    if (login(username, password, role) == 1) { // Jika pengguna memasukkan data yang benar
         // Memuat data alat dari file setelah login berhasil
-        MuatAlat(alat, &jumlahAlat); //Fungsi ini membaca file yang berisi informasi alat.
+        MuatAlat(alat, &jumlahAlat); // Fungsi ini membaca file yang berisi informasi alat.
         
         if (strcmp(role, "admin") == 0) {
             adminMenu(alat, &jumlahAlat);
